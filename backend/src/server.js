bb230daa34dad0689070298fc1721dc55d3bbb50
JigyasuT <jigyasuti@gmail.com>
2025-08-04 13:50:00 +0530

    oiii

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const { default: serverless } = require('serverless-http');
+const serverless = require('serverless-http');
 
 
 
